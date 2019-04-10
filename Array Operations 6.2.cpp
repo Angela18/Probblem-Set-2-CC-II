d7c3bc21964456cfca80439bf4595f37e99704bb
Angela Aquise Santos <Angela18@users.noreply.github.com>
2019-04-10 10:20:12 -0500

    Add files via upload

---
+++ Array Operations 6.2.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+
+using namespace std;
+
+void reverse(int numbers[], const int numbersLen){
+    for(int i=0;i<numbersLen/2;++i){
+        int tmp=numbers[i];
+        int indexFromEnd=numbersLen-i-1;
+        numbers[i]=numbers[indexFromEnd];
+        numbers[indexFromEnd]=tmp;
+    }
+}
+
+int main(){
+    int numbersLen=5;
+    int arr[numbersLen]={1,2,3,4,5};
+    cout<<"Array Original: ";
+    for (int i=0;i<numbersLen;i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+    reverse(arr,numbersLen);
+    cout<<"Array Invertido: ";
+    for (int i=0;i<numbersLen;i++){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
